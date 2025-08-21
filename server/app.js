899997fc40890f3af44b4099a5f438a7a3ec882f
Harshiniii027 <gmdhguruharshini@gmail.com>
2025-08-21 15:46:24 +0530

    ready to deploy

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 require("dotenv").config();
 require("./connection/conn");
 app.use(cors({
-    origin:["http://localhost:5173"],
+    origin:["*"],
     credentials: true,
 }
 )); 
